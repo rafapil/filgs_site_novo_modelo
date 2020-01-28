3745771010fdaacbb31ad2409b7e683d7d743307
Rafael Filgueiras <rafasera@Rafaels-MacBook-Pro.local>
2020-01-27 22:13:17 -0300

    Ajustado para PHP e removido artefatos desnecessarios

---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Filgs &mdash; Consultoria em Monitoramento e Desenvolvimento</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    
+    
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+    <div class="site-navbar-wrap">
+      
+      <div class="site-navbar site-navbar-target js-sticky-header">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-6 col-md-4">
+              <h1 class="my-0 site-logo"><a href="index.html">Filgs<span class="text-primary">.</span> </a></h1>
+            </div>
+            <div class="col-6 col-md-8">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+
+                  <div class="d-inline-block d-lg-block ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black">
+                    <span class="icon-menu h3"></span> <span class="menu-text">Menu</span>
+                  </a></div>
+
+                  <ul class="site-menu main-menu js-clone-nav d-none d-lg-none">
+                    <li><a href="#home-section" class="nav-link">Home</a></li>
+                    <li><a href="#what-we-do-section" class="nav-link">Em que Atuamos</a></li>
+                    <li><a href="#about-section" class="nav-link">Sobre a Consultoria</a></li>
+                    <li><a href="#portfolio-section" class="nav-link">Portfolio</a></li>
+                    <li><a href="#studio-section" class="nav-link">Gallery</a></li>
+                    <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                    <li><a href="#login.html" class="nav-link">Login</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- END .site-navbar-wrap -->
--- index.php
+++ index.php
@@ -1,77 +1,15 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <title>Filgs &mdash; Consultoria em Monitoramento e Desenvolvimento</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    
-    
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+include_once('header.php');
+
+?>
+
+
   
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-    
-
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-  
-
-
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div> <!-- .site-mobile-menu -->
     
     
-    <div class="site-navbar-wrap">
-      
-      <div class="site-navbar site-navbar-target js-sticky-header">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-6 col-md-4">
-              <h1 class="my-0 site-logo"><a href="index.html">Filgs<span class="text-primary">.</span> </a></h1>
-            </div>
-            <div class="col-6 col-md-8">
-              <nav class="site-navigation text-right" role="navigation">
-                <div class="container">
-
-                  <div class="d-inline-block d-lg-block ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black">
-                    <span class="icon-menu h3"></span> <span class="menu-text">Menu</span>
-                  </a></div>
-
-                  <ul class="site-menu main-menu js-clone-nav d-none d-lg-none">
-                    <li><a href="#home-section" class="nav-link">Home</a></li>
-                    <li><a href="#what-we-do-section" class="nav-link">Em que Atuamos</a></li>
-                    <li><a href="#about-section" class="nav-link">Sobre a Consultoria</a></li>
-                    <li><a href="#portfolio-section" class="nav-link">Portfolio</a></li>
-                    <li><a href="#studio-section" class="nav-link">Gallery</a></li>
-                    <li><a href="#contact-section" class="nav-link">Contact</a></li>
-                    <li><a href="#login.html" class="nav-link">Login</a></li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> <!-- END .site-navbar-wrap -->
     
+    <!-- Inicio do bloco pertencente ao carrocel pra cima -->
     <div class="site-blocks-cover" id="home-section">
       <div class="img-wrap">
         <div class="owl-carousel slide-one-item hero-slider">
@@ -85,7 +23,7 @@
             <img src="images/glint_hero_3.jpg" alt="Image">  
           </div>
         </div>
-      </div>
+      </div> <!-- Fim do bloco do carrocel -->
       <div class="container">
         <div class="row">
           <div class="col-md-6 ml-auto align-self-center">
@@ -94,7 +32,7 @@
                 <h1>Filgs Consultoria</h1>
               </div>
               <div class="text">
-                <p class="sub-text mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde modi blanditiis iure.</p>
+                <p class="sub-text mb-5">Base Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde modi blanditiis iure.</p>
                 <p><a href="#" target="_blank" class="btn btn-outline-primary btn-md">Inicie um projeto com a gente</a></p>
               </div>
             </div>
@@ -201,63 +139,6 @@
       </div>
     </div> <!-- .END site-section -->
 
-<!--
-    <div class="site-section" id="what-we-do-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-lg-6 section-title">
-            <span class="sub-title mb-2 d-block">Nosso Time</span>
-            <h2 class="title text-primary">Não somos apenas talentos, somos apaixonados por tudo isso</h2>
-          </div>
-        </div>
-        <div class="row no-gutters">
-          <div class="col-lg-6 mb-5 person">
-            <img src="images/person_1.jpg" alt="Image" class="img-fluid mb-5">
-
-            <div class="row">
-              <div class="col-lg-10 ml-auto">
-                <div class="pr-lg-5">
-                  <h3>Jacob Spencer</h3>
-                  <span class="mb-4 d-block">CEO, Co-Founder</span>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                  <p class="d-flex align-items-center">
-                    <span class="mr-3 text-black">Follow me:</span>
-                    <a href="#" class="twitter pr-2 pt-2 pb-2 pl-0"><span class="icon-twitter"></span></a>
-                    <a href="#" class="facebook p-2"><span class="icon-facebook"></span></a>
-                    <a href="#" class="dribbble p-2"><span class="icon-dribbble"></span></a>
-                    <a href="#" class="instagram p-2"><span class="icon-instagram"></span></a>
-                    <a href="#" class="linkedin p-2"><span class="icon-linkedin"></span></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 person">
-            <img src="images/person_2.jpg" alt="Image" class="img-fluid mb-5">
-            <div class="row">
-              <div class="col-lg-10 ml-auto">
-                <div class="pr-lg-5">
-                  <h3>Chris Peters</h3>
-                  <span class="mb-4 d-block">CTO, Co-Founder</span>
-                  <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-                  <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-                  <p class="d-flex align-items-center">
-                    <span class="mr-3 text-black">Follow me:</span>
-                    <a href="#" class="twitter pr-2 pt-2 pb-2 pl-0"><span class="icon-twitter"></span></a>
-                    <a href="#" class="github p-2"><span class="icon-github"></span></a>
-                    <a href="#" class="dribbble p-2"><span class="icon-dribbble"></span></a>
-                    <a href="#" class="instagram p-2"><span class="icon-instagram"></span></a>
-                    <a href="#" class="linkedin p-2"><span class="icon-linkedin"></span></a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
- -->   
 
     <div class="site-section" id="portfolio-section">
       <div class="container">
@@ -558,99 +439,7 @@
       </div>
     </div> <!-- END .site-section -->
   
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 mb-5 mb-lg-0">
-            <div class="row mb-5">
-              <div class="col-12">
-                <h3 class="footer-heading mb-4">About Us</h3>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-              </div>
-            </div>
-            
-
-            
-          </div>
-          <div class="col-lg-3 ml-auto">
-           
-            <div class="row mb-5">
-              <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Navigation</h3>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                <ul class="list-unstyled">
-                  <li><a href="#home-section" class="smoothscroll">Home</a></li>
-                  <li><a href="#what-we-do-section" class="smoothscroll">What We Do</a></li>
-                  <li><a href="#about-section" class="smoothscroll">About Us</a></li>
-                  <li><a href="#team-section"  class="smoothscroll">Team</a></li>
-                </ul>
-              </div>
-              
-            </div>
-            
-          </div>
-          
-
-          <div class="col-lg-4 mb-5 mb-lg-0">
-
-            <div class="mb-5">
-              <h3 class="footer-heading mb-2">Subscribe Newsletter</h3>
-              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-
-              <form method="post" class="form-subscribe">
-                <div class="form-group mb-3">
-                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Enter full name" aria-label="Enter Email" aria-describedby="button-addon2">
-                </div>
-                <div class="form-group mb-3">
-                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Enter email" aria-label="Enter Email" aria-describedby="button-addon2">
-                </div>
-                <div class="form-group">
-                  <button class="btn btn-primary px-5" type="submit">Subscribe</button>
-                </div>
-              </form>
-
-            </div>
-
-            
-
-
-          </div>
-          
-        </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="mb-4">
-                  <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-                </div>
-            <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-          
-        </div>
-      </div>
-    </footer>
-  </div>
-
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.sticky.js"></script>
-  <script src="js/stickyfill.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/isotope.pkgd.min.js"></script>
-  
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/main.js"></script>
-
-     
-  </body>
-</html>
+    <?php
+    include_once('footer.php');
+    
+    ?>
